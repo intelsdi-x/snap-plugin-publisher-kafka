594a241c004448b2aa8c18430d8004282138c378
Haralds <haralds@dreamdraco.com>
2016-11-15 12:12:46 +0000

    fixed formating

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -35,9 +35,9 @@ import (
 )
 
 const (
-	PluginName = "kafka"
+	PluginName    = "kafka"
 	PluginVersion = 10
-	PluginType = plugin.PublisherPluginType
+	PluginType    = plugin.PublisherPluginType
 )
 
 func Meta() *plugin.PluginMeta {
@@ -52,12 +52,12 @@ func NewKafkaPublisher() *kafkaPublisher {
 
 type MetricToPublish struct {
 	// The timestamp from when the metric was created.
-	Timestamp          time.Time         `json:"timestamp"`
-	Namespace          string            `json:"namespace"`
-	Data               interface{}       `json:"data"`
-	Unit               string            `json:"unit"`
-	Tags               map[string]string `json:"tags"`
-	Version_           int               `json:"version"`
+	Timestamp time.Time         `json:"timestamp"`
+	Namespace string            `json:"namespace"`
+	Data      interface{}       `json:"data"`
+	Unit      string            `json:"unit"`
+	Tags      map[string]string `json:"tags"`
+	Version_  int               `json:"version"`
 	// Last advertised time is the last time the snap agent was told about a metric.
 	LastAdvertisedTime time.Time `json:"last_advertised_time"`
 }
@@ -108,15 +108,15 @@ func (k *kafkaPublisher) Publish(contentType string, content []byte, config map[
 	case "array":
 		jsonOut, err = json.Marshal(metrics)
 	case "tree":
-		if (len(metrics) > 0) {
+		if len(metrics) > 0 {
 			var timestamp interface{} = metrics[0].Timestamp.UnixNano()
 			var tags interface{} = metrics[0].Tags
-			i2 := makeTree(metrics).(map[string]*interface{});
-			i2 ["_timestamp"] = &timestamp
-			i2 ["_tags"] = &tags
+			i2 := makeTree(metrics).(map[string]*interface{})
+			i2["_timestamp"] = &timestamp
+			i2["_tags"] = &tags
 			jsonOut, err = json.Marshal(i2)
 		} else {
-			jsonOut,err = json.Marshal(make(map[string]string))
+			jsonOut, err = json.Marshal(make(map[string]string))
 		}
 
 	default:
@@ -159,7 +159,7 @@ func (k *kafkaPublisher) publish(topic string, brokers []string, content []byte,
 			panic(err)
 		}
 	}()
-	if (key == "") {
+	if key == "" {
 		_, _, err = producer.SendMessage(&sarama.ProducerMessage{
 			Topic: topic,
 			Value: sarama.ByteEncoder(content),
@@ -169,24 +169,24 @@ func (k *kafkaPublisher) publish(topic string, brokers []string, content []byte,
 		_, _, err = producer.SendMessage(&sarama.ProducerMessage{
 			Topic: topic,
 			Value: sarama.ByteEncoder(content),
-			Key: sarama.ByteEncoder(key),
+			Key:   sarama.ByteEncoder(key),
 		})
 		return err
 	}
 
 }
 
-func makeTree(metrics [] MetricToPublish) interface{} {
+func makeTree(metrics []MetricToPublish) interface{} {
 	var out interface{} = make(map[string]*interface{})
 	for _, v := range metrics {
 		s := strings.Split(v.Namespace, "/")[1:]
 		putData(&out, s, &v.Data)
 	}
-	return out;
+	return out
 }
 
 func putData(i1 *interface{}, path []string, data *interface{}) {
-	i2 := (*i1).(map[string]*interface{});
+	i2 := (*i1).(map[string]*interface{})
 	if len(path) == 1 {
 		i2[path[0]] = data
 	} else {
@@ -230,4 +230,3 @@ func handleErr(e error) {
 		panic(e)
 	}
 }
-
